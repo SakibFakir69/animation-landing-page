1dbffeda8c02147bbab18a5f178b6797824d0ea2
Sakib Fakir <FakirSakib22232@gmail.com>
2025-11-03 16:59:31 +0800

    animation project setup done

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <main className="p-10">
+        <h1 className="text-4xl font-general text-violet-300 ">
+
+          Welcome to award-winning website
+        </h1>
+        <p className='font-circular-web'>font</p>
+        <h1 className="font-robert-regular text-2xl">Regular Font</h1>
+<h1 className="font-robert-medium text-2xl ">Medium Font</h1>
+
+      </main>
+    </div>
+  )
+}
+
+export default App
